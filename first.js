9600eb989ad91b57b4968dc156ebfcb58654d2a4
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-06-13 20:05:29 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,26 @@
+let big=document.getElementsByClassName("big");
+
+
+
+
+let ele=document.getElementById("red");
+ele.onclick=function(){
+    big[0].style.backgroundColor="red";
+}
+
+let el=document.getElementById("yellow");
+el.onclick=function(){
+    big[0].style.backgroundColor="yellow";
+}
+
+let a=document.getElementById("blue");
+a.onclick=function(){
+    big[0].style.backgroundColor="blue";
+}
+
+let b=document.getElementById("green");
+b.onclick=function(){
+    big[0].style.backgroundColor="green";
+}
+
+
